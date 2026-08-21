95e09e83619f7f04368e5b410a450abfa4f41fa8
Mohamed Essam <mo7am3d.tube@gmail.com>
2026-08-21 16:04:42 +0300

    Updare AssetModel fIle

--- src/routes/data.py
+++ src/routes/data.py
@@ -93,23 +93,38 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
   
   project = await project_model.get_project_or_create_one(project_id=project_id)
   
-  project_files_ids = []
-  if process_request.file_id:
-    project_files_ids = [process_request.file_id]
-  else:
-    asset_model = await AssetModel.create_instance(
+  asset_model = await AssetModel.create_instance(
       db_client=request.app.db_client
     )
+  
+  project_files_ids = {}
+  if process_request.file_id:
+    asset_record = await asset_model.get_asset_record(
+      asset_project_id=project.id,
+      asset_name=process_request.file_id
+    )
     
+    if asset_record is None:
+      return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={
+          "signal": ResponseSignal.FILE_ID_ERROR.value,
+        }
+      )
+    
+    project_files_ids = {
+      asset_record.id: asset_record.asset_name
+    }
+  else:
     project_files = await asset_model.get_all_project_assets(
       asset_project_id=project.id,
       asset_type=AssetTypeEnum.FILE.value
     )
     
-    project_files_ids = [
-      record["asset_name"]
+    project_files_ids = {
+      record.id: record.asset_name
       for record in project_files
-    ]
+    }
   
   if len(project_files_ids) == 0:
     return JSONResponse(
@@ -131,7 +146,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
       project_id=project.id
     )
   
-  for file_id in project_files_ids:
+  for asset_id, file_id in project_files_ids.items():
     file_content = processcontroller.get_file_cotent(file_id=file_id)
     
     if file_content is None:
@@ -158,7 +173,8 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
         chunk_text=chunk.page_content,
         chunk_metadata=chunk.metadata,
         chunk_order=i+1,
-        chunk_project_id=project.id
+        chunk_project_id=project.id,
+        chunk_asset_id = asset_id
       )
       for i, chunk in enumerate(file_chunks)
     ]
